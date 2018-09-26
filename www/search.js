6ebcd64eb92502d76f3271a87e0802ab5e9abf6f
LauresLindroos <laurea.lindroos@hotmail.com>
2018-09-26 09:18:30 +0200

    adding comments in search.js

--- www/search.js
+++ www/search.js
@@ -1,8 +1,11 @@
 //Searching for recipe
+
+//function for button
  $('#search-button').click(()  =>{
     let searchValue = $('#search-value').val();
     getRecipes(searchValue);
 })
+//Fetch data from JSON-file
 function getRecipes(searchValue){
     
     $.get('http://localhost:3000/recipes/' + searchValue, (data) => {
@@ -12,11 +15,14 @@ function getRecipes(searchValue){
     });
     
 }
+//Display data on page
 function writeRecipes(recipe) {
 
     
     $('#search-result').append(`<h4> ${recipe.name} </h4>`);
     $('#search-result').append(`<h5> ${recipe.instructions}</h5>`);
+
+    //To search for ingredients we have to go to an object within the object in JSON file
     let ingredients;
     for(let i of recipe.ingredients){
         $('#search-result').append(`<h5> ${i.name}</h5>`);
@@ -24,31 +30,3 @@ function writeRecipes(recipe) {
 
 }
 
-/* $('#search-button').on('click', async function(){
-    let searchinput = $('#search-value').val().toLowerCase();
-    recipes = await $.getJSON('/recipe.json').catch(console.err);
-    let match = false;
-    let result;
-    for(let r of recipes){
-        if(searchinput == r.name.toLowerCase()){
-            console.log(r.name)
-            match = true;
-            result = r;
-        }    
-    }
-    if(match){
-        $('#search-result').text(result.name);
-        $('#search-result').append('<br>');
-    
-        let ingredientlist;
-        for(let i of result.ingredientlist){
-            $('#search-result').append(i.name + " ");
-            $('#search-result').append(i.units);
-            $('#search-result').append(i.measuringUnit);
-            $('#search-result').append('<br>');
-        }
-    }
-    else{
-     $('#search-result').text("Inget recept med det namnet hittades");
-    }
-}); */
