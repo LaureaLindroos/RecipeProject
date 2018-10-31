9f4b8bc6bbc2285a5187b942147bb25f371d8c61
Laurea <laurea.lindroos@hotmail.com>
2018-10-31 15:47:38 +0100

    removed unneccessary classes and centered form on index

--- www/helpers/add_recipe_data.js
+++
@@ -1,9 +0,0 @@
-class AddRecipeData {
-    constructor(name, portions, instructions, ingredients, url) {
-        let name = name;
-        let portions = portions;
-        let instructions = [];
-        let ingredients = [];
-        let ImgUrl = ImgUrl;
-    }
-}
